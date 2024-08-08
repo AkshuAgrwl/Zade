9107b6f5e312b1bbf1053b94e2663bbaffe73d83
Akshit Agarwal <139654279+AkshuAgrwl@users.noreply.github.com>
2024-08-08 15:55:55 +0530

    Fix supertokens backend config

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from supertokens_python.recipe import emailpassword, dashboard
+from supertokens_python.recipe import emailpassword, dashboard, session
 from supertokens_python.framework.fastapi import get_middleware
 from supertokens_python import (
     init as st_init,
@@ -17,16 +17,13 @@ from helpers.router import DirRouter
 
 app = FastAPI()
 
-router = DirRouter(Path(__file__).parent.joinpath("app"))  # ./app directory
-app.include_router(router)
-
 st_init(
     app_info=InputAppInfo(
         app_name=os.environ["APP_NAME"],
         api_domain="http://localhost:8000",
         api_base_path="/auth/",
         website_domain=os.environ["SITE_URL"],
-        website_base_path="/auth",
+        website_base_path="/",
     ),
     supertokens_config=SupertokensConfig(
         connection_uri=os.environ["SUPERTOKENS_CONNECTION_URI"]
@@ -35,11 +32,16 @@ st_init(
     recipe_list=[
         emailpassword.init(),
         dashboard.init(),
+        session.init(),
     ],
     mode="asgi",
 )
 
 app.add_middleware(get_middleware())
+
+router = DirRouter(Path(__file__).parent.joinpath("app"))  # ./app directory
+app.include_router(router)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[os.environ["SITE_URL"]],
